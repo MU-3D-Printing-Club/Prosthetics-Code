5b070aac315c638f7f3b7e8a8f40f207a844de6c
Christopher Scully <scullycphone@gmail.com>
2019-10-11 15:06:48 -0500

    reframe for multiple gestures, need to set functions to set position and new wiring

--- myoFlex/myoFlex.ino
+++ myoFlex/myoFlex.ino
@@ -1,70 +1,115 @@
 //this code takes input from a myoelctric and toggles the hand between the contracted
 // and extended positions when there is a spike above a user-defined level
-// future projects will include running this code with low power 
-//jd is cool
-//jd is cooler
-//aw;oifjaewoif;j
-
+// future projects will include running this code with low power
+ 
 #include <Servo.h>     //library used for servos
-
+ 
 Servo thumb;            //defines the name of the five servos and them as servo objects
 Servo pointer;
 Servo middle;
 Servo ring;
 Servo pinky;
-
+ 
 #define min 2000    //static integers of minimum and maximum servo extension
 #define max 1000
+int trigger 600 //change later with new function
 
 #define thumbPin 3    //these are the PWM pins where servos are connected
 #define pointerPin 5
 #define middlePin 6
 #define ringPin 9
 #define pinkyPin 10
-
-
+ 
+int state = 0;
 float currentVoltage=0;   //this is where to store each read from the myo
 #define threshold .4    //this is the percentage of the max read we use to define a flex
 #define myoIn A1      //the pin where myo signal comes in
 
-bool contract=false;   //variable we will use to toggle action
-
+int setTrigger();
+int trigger(int);
+int setOpen();
+int setPoint();
+int setPinch();
+int setFist();
+ 
 // THIS PART ONLY RUNS ONCE
 void setup() {
 Serial.begin(9600);     //starts up serial communication between arduino and computer
-  thumb.attach(thumbPin, min, max);    //defines where the servos are and their max/mins
-  pointer.attach(pointerPin, min, max);
-  middle.attach(middlePin, min, max);
-  ring.attach(ringPin, min, max);
-  pinky.attach(pinkyPin, min, max);
-  Serial.println("Begin");    //prints a start message
-  handPosition(max,max,max,max,max);       //makes the hand relax all of the way
+  Serial.println("Starting");       //makes the hand relax all of the way
+  trigger = setTrigger();
+  state = 1;
 }
-
-// THIS PART OF THE CODE LOOPS CONTINIOUSLY 
+ 
+// THIS PART OF THE CODE LOOPS CONTINIOUSLY
 void loop() {
-   currentVoltage = analogRead(myoIn);     //reads myoelectric input
-   currentVoltage=currentVoltage/1023;      // converts from 0-1023 scale to 0-1 scale
-   Serial.println(currentVoltage);        //puts it in the serial moniter
-   if (contract==false && currentVoltage>threshold){      //if we see a spike and the hand is relaxed
-    Serial.println("Contract!");     
-    handPosition(min,min,min,min,min);            //make it squeeze; see subfunction below loop body
-    contract=true;                                 //change the boolean
-   }
-   else if (contract==true && currentVoltage>threshold){   //if it is already contracted then make it relax
-    Serial.println("Extend!");
-    handPosition(max,max,max,max,max);
-    contract=false;
-   }
+if(trigger(currentVoltage = analogRead(myoIn))){
+  state++;
+  state = state%5;
+  //Serial.println(currentVoltage);        //puts it in the serial moniter
+  if(state == 1){
+    if(!setOpen()){
+        Serial.println("Failed");
+    }
+    else{
+      Serial.println("open");
+    }
+  }
+  else if(state == 2){
+      if(!setPoint()){
+        Serial.println("Failed");
+      }
+    else{
+      Serial.println("point");
+    }
+  }
+  else if(state == 3){
+    if(!setPinch()){
+      Serial.println("Failed");
+    }
+    else{
+      Serial.println("pinch");
+    }
+  }
+  else {
+    if(state == 4){
+    if(!setFist()){
+      Serial.println("Failed");
+    }
+    else{
+      Serial.println("fist");
+    }
+  }
+  }
 }
 
-void handPosition(int thumbPos,int pointerPos,int middlePos,int ringPos,int pinkyPos){
-  //function hand position takes in 5 integer inputs for each finger and moves the servos according to
-  //each input. for the servos, maximum extension is 1000 and minimum (all the way in) is 2000.
-  thumb.writeMicroseconds(thumbPos);   //this is a function from the servo.h library basically gives the pulse width
-  pointer.writeMicroseconds(pointerPos);
-  middle.writeMicroseconds(middlePos);
-  ring.writeMicroseconds(ringPos);
-  pinky.writeMicroseconds(pinkyPos);
-  delay(1000);       //this is so the servos have time to get to their set position
+int setTrigger(){
+  float average = 0;
+
+  //build collection here
+
+  return (int)average;
+}
+
+int trigger(int voltage){
+  if(voltage > trigger){
+    return 1;
+  }
+  return 0;
+}
+ 
+int setOpen(){
+  //build open
+  return 1;
+}
+int setPoint(){
+  //build point
+  return 1;
+}
+int setPinch(){
+  //bluild pinch
+  return 1;
+}
+int setFist(){
+  //build fist
+ return 1;
 }
