4462baa183eb5baf32e2f9dfee1a509bb6802b73
Benjamin Chilson <benjameschilson@gmail.com>
2019-11-10 14:17:28 -0600

    setTrigger() to be more precise
    Added variables to easily tweak intervals and time of trials. Also made the interval for each reading smaller so more information is collected for more accurate triggers

--- myoFlex/myoFlex.ino
+++ myoFlex/myoFlex.ino
@@ -1,97 +1,3 @@
-<<<<<<< Updated upstream
-//this code takes input from a myoelctric and toggles the hand between the contracted
-// and extended positions when there is a spike above a user-defined level
-// future projects will include running this code with low power
-//jd is cool
-//jd is cooler
-
-
-#include <Servo.h>     //library used for servos
-
-Servo thumb;            //defines the name of the five servos and them as servo objects
-Servo pointer;
-Servo middle;
-Servo ring;
-Servo pinky;
-
-#define min 2000    //static integers of minimum and maximum servo extension
-#define max 1000
-
-#define thumbPin 3    //these are the PWM pins where servos are connected
-#define pointerPin 5
-#define middlePin 6
-#define ringPin 9
-#define pinkyPin 10
-#define buttonPin 0 //This is the pin the button is located(NEEDS TO BE CHANGED TO REFLECT THE ACTUAL LOCATION ON THE HARDWARE)
-
-
-float currentVoltage=0;   //this is where to store each read from the myo
-#define threshold .4    //this is the percentage of the max read we use to define a flex
-#define myoIn A1      //the pin where myo signal comes in
-
-bool contract=false;   //variable we will use to toggle action
-int buttonState = 0;  //variable for reading the pushButton status
-
-// THIS PART ONLY RUNS ONCE
-void setup() {
-  Serial.begin(9600);     //starts up serial communication between arduino and computer
-  thumb.attach(thumbPin, min, max);    //defines where the servos are and their max/mins
-  pointer.attach(pointerPin, min, max);
-  middle.attach(middlePin, min, max);
-  ring.attach(ringPin, min, max);
-  pinky.attach(pinkyPin, min, max);
-
-  pinMode(buttonPin, INPUT);  //initializes buttonPin as an input
-  Serial.println("Begin");    //prints a start message
-  handPosition(max,max,max,max,max);       //makes the hand relax all of the way
-}
-
-// THIS PART OF THE CODE LOOPS CONTINIOUSLY
-void loop() {
-  buttonState = digitalRead(buttonPin);   //reads the state of the pushButton value
-  currentVoltage = analogRead(myoIn);     //reads myoelectric input
-  currentVoltage=currentVoltage/1023;      // converts from 0-1023 scale to 0-1 scale
-  Serial.println(currentVoltage);        //puts it in the serial moniter
-
-  if (contract==false && currentVoltage>threshold){      //if we see a spike and the hand is relaxed
-    Serial.println("Contract!");
-    handPosition(min,min,min,min,min);            //make it squeeze; see subfunction below loop body
-    contract=true;                                 //change the boolean
-  }else if (contract==true && currentVoltage>threshold){   //if it is already contracted then make it relax
-    Serial.println("Extend!");
-    handPosition(max,max,max,max,max);
-    contract=false;
-  }
-
-  if(buttonState == HIGH){
-    if (contract==false && currentVoltage>threshold){      //if we see a spike and the hand is relaxed
-      Serial.println("Contract!");
-      handPosition(min,min,min,min,min);            //make it squeeze; see subfunction below loop body
-      contract=true;                                 //change the boolean
-    }else if (contract==true && currentVoltage>threshold){   //if it is already contracted then make it relax
-      Serial.println("Extend!");
-      handPosition(max,max,max,max,max);
-      contract=false;
-    }
-  }
-  else{
-    Serial.println("The hand is now turned off");
-  }
-
-
-}
-
-void handPosition(int thumbPos,int pointerPos,int middlePos,int ringPos,int pinkyPos){
-  //function hand position takes in 5 integer inputs for each finger and moves the servos according to
-  //each input. for the servos, maximum extension is 1000 and minimum (all the way in) is 2000.
-thumb.writeMicroseconds(thumbPos);   //this is a function from the servo.h library basically gives the pulse width
-  pointer.writeMicroseconds(pointerPos);
-  middle.writeMicroseconds(middlePos);
-  ring.writeMicroseconds(ringPos);
-  pinky.writeMicroseconds(pinkyPos);
-  delay(1000);       //this is so the servos have time to get to their set position
-}
-=======
 //this code takes input from a myoelctric and toggles the hand between the contracted
 // and extended positions when there is a spike above a user-defined level
 // future projects will include running this code with low power
@@ -171,31 +77,40 @@ void loop() {
   }
 }
 
-//the loop function will continously check the users input and assign it to currentVoltage, then it's verified through the check function to see if its higher than the set trigger value from setup, if so then it will switch between the four hand positions in a looping structure
 
+//Finds the max flex for a total of 3 trials
 int setTrigger(){
 
   float average;
   do{
-    //Finds the max flex for a total of 3 trials
-    float sumMaxFlexOfTrials = 0;
+    float sumMaxFlexOfTrials = 0; //the sum of the max reading found for each of the three trials
     
-    for(int j = 1; j <= 3; j++){  //loops to collect the three impulses
+    for(int j = 1; j <= 3; j++){  //loops to collect the max of the three trials
       Serial.print("Reading max of Flex ");
       Serial.print(j);
       Serial.println(" reading during the next 3 seconds");
+
+      int intervalOfEachTrial = 3000; //interval time in milliseconds that each trial happens
+      int intervalOfReadings = 10; //interval to compare readings in milliseconds
       float maxRecordedFlex = 0;
-      for(int i = 3000; i != 0; i -= 300){  //records values every 1/5 of a second and keeps track of which reading was the max
-        float reading1 = analogRead(myoIn);
-        delay(100);
-        float reading2 = analogRead(myoIn);
-        delay(100);
+      
+      //collects the first two readings of the trial
+      float reading1 = analogRead(myoIn);
+      delay(intervalOfReadings);
+      float reading2 = analogRead(myoIn);
+      delay(intervalOfReadings);
+
+      int remainderOfTrial = intervalOfEachTrial - intervalOfReadings;
+      for(int i = remainderOfTrial; i > 0; i -= intervalOfReadings){  //for the remainder of the trial collects the next data point and checks if the previous data point was the absolute max
         float reading3 = analogRead(myoIn); 
         float currentReading = reading2;
         
-        if(currentReading > reading1 && currentReading < reading3 && currentReading > maxRecordedFlex)
+        if(currentReading > reading1 && currentReading < reading3 && currentReading > maxRecordedFlex) //checks if currentReading is a relative max, then checks if it is the absolute max
           maxRecordedFlex = currentReading;
-        delay(100);
+          
+        delay(intervalOfReadings);
+        reading1 = reading2;
+        reading2 = reading3;
       }
 
       sumMaxFlexOfTrials += maxRecordedFlex; //Adds the max of this trial to the sum of the maxes of all the trials
@@ -234,4 +149,3 @@ void handPosition(int thumbPos,int pointerPos,int middlePos,int ringPos,int pink
 }
 
 //handPosition takes 5 integer values to be set to each servo for the finger, with one second of time to adjust, their position is read and if not accurate, will adjust servo command to stay at the position
->>>>>>> Stashed changes
